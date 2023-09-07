0c87fe4812fa3ee447e08891d48277b048fbbe9f
apta-tamilarasi <athinarayanantamilarasi@gamil.com>
2023-09-07 21:37:50 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,19 @@
+let a=parseInt(prompt("Enter the number"))
+
+document.write("M value is "+a+"<br>")
+
+if(a>0){
+	document.write("The value of n is 1")
+}
+
+else if(a<0){
+	document.write("The value of n is -1")
+}
+
+else if(a==0){
+	document.write("The value of n is 0")
+}
+
+else{
+	document.write("Pls enter valid number")
+}
